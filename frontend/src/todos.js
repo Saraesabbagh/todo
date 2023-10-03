8fe21af694ec4bf04a48b1858cdc86ef3d55ce9e
Sara  Sabbagh <sarasabbagh@saras-mbp.home>
2023-10-03 11:43:25 +0100

    done most of basic styling

--- frontend/src/todos.js
+++ frontend/src/todos.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './todos.css';
+import listiclelogoandname from "./images/listiclelogoandname.png";
+// import listiclename from "./images/listiclename.png";
 
 function Todos() {
   const [todos, setTodos] = useState([]);
@@ -17,17 +19,38 @@ function Todos() {
 
     getTodos();
   }, []);
+//   const handleCheckboxChange = async (todoId, done) => {
+//     try {
+//       // Update the todo's done property in the backend
+//       // Implement your logic to update the todo's done property here
+//       console.log(`Todo ID: ${todoId}, Done: ${done}`);
+//     } catch (error) {
+//       console.error('Error updating todo:', error);
+//     }
+//   };
 
   return (
     <div>
-      <h1>Welcome to the frontend Todo App</h1>
+    <div className='logoContainer'>
+      <img src={listiclelogoandname} alt="ListicleLogo and name" />
+      </div>
+    <div className='container'>
+        <div className='titleButtonContainer'>
+            <span className="inline-container"><h2 >My Todos</h2><button className='addButton'>Add Todo</button></span>
+        </div>
+      
       <ul>
         {todos.map(todo => (
           <li key={todo._id}>
-            {todo.title} - {todo.description}
+            {todo.title} - {todo.description} - <input
+                type="checkbox"
+                checked={todo.done} // Set checkbox state based on the todo's done property
+                // onChange={() => handleCheckboxChange(todo._id, !todo.done)}
+              />
           </li>
         ))}
       </ul>
+      </div>
     </div>
   );
 }
