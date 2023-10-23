47db51266e9fa009301f513f6f0eaf86a796679e
saraesabbagh <sarasabbagh@hotmail.co.uk>
2023-10-23 10:10:34 +0100

    added delete functionality

--- backend/controllers/todo.js
+++ backend/controllers/todo.js
@@ -1,37 +1,45 @@
 const Todo = require("../models/Todo");
 
-exports.addtodo =async (req,res)=>{
+exports.addtodo = async (req, res) => {
   try {
     const {
-        title,
-        description,
-        deadlineYear,
-        deadlineMonth,
-        deadlineDay,
-        done,
-      }= req.body
-      const todo = await new Todo({
-        title,
-        description,
-        deadlineYear,
-        deadlineMonth,
-        deadlineDay,
-        done,
-      }).save();
-      res.json(todo);
-    }catch (error) {
+      title,
+      description,
+      deadlineYear,
+      deadlineMonth,
+      deadlineDay,
+      done,
+    } = req.body;
+    const todo = await new Todo({
+      title,
+      description,
+      deadlineYear,
+      deadlineMonth,
+      deadlineDay,
+      done,
+    }).save();
+    res.json(todo);
+  } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
 
 exports.getTodos = async (req, res) => {
-    try {
-      // Retrieve all todos from the database
-      const todos = await Todo.find();
-      res.json(todos); // Send the array of todos as a JSON response
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
-  };
+  try {
+    // Retrieve all todos from the database
+    const todos = await Todo.find();
+    res.json(todos); // Send the array of todos as a JSON response
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
-
+exports.deletetodo = async (req, res) => {
+  const { entryId } = req.body;
+  try {
+    await Todo.findByIdAndDelete(entryId);
+    res.status(200).json({ message: "Entry deleted successfully." });
+  } catch (error) {
+    res.status(500).json({ error: "Error deleting entry." });
+  }
+};
--- frontend/src/todos.js
+++ frontend/src/todos.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import './todos.css';
+import React, { useState, useEffect } from "react";
+import { MdDelete } from "react-icons/md";
+import "./todos.css";
 import Modal from "./modal";
 import listiclelogoandname from "./images/listiclelogoandname.png";
 // import listiclename from "./images/listiclename.png";
@@ -7,60 +8,82 @@ import listiclelogoandname from "./images/listiclelogoandname.png";
 function Todos() {
   const [todos, setTodos] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
-  
-
-  
-
 
   useEffect(() => {
     const getTodos = async () => {
       try {
-        const res = await fetch('http://localhost:8000/todos');
+        const res = await fetch("http://localhost:8000/todos");
         const data = await res.json();
         const reversedTodos = data.reverse();
         setTodos(reversedTodos);
       } catch (error) {
-        console.error('Error fetching todos:', error);
+        console.error("Error fetching todos:", error);
       }
     };
 
     getTodos();
   }, []);
 
+  const handleDeleteClick = async (entryId) => {
+    try {
+      const response = await fetch("http://localhost:8000/deletetodo", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ entryId: entryId }),
+      });
+
+      if (response.ok) {
+        const updatedTodos = todos.filter((todo) => todo._id !== entryId);
+        setTodos(updatedTodos);
+      } else {
+        throw new Error("Failed to delete entry");
+      }
+    } catch (error) {
+      console.error("Error deleting entry:", error);
+    }
+  };
+
   return (
     <div>
-    <div className='logoContainer'>
-      <img src={listiclelogoandname} alt="ListicleLogo and name" />
+      <div className="logoContainer">
+        <img src={listiclelogoandname} alt="ListicleLogo and name" />
       </div>
-    <div className='container'>
-        <div className='titleButtonContainer'>
-            <span className="inline-container"><h2 >My Todos</h2><button className='addButton' onClick={() => setIsModalOpen(true)}>
+      <div className="container">
+        <div className="titleButtonContainer">
+          <span className="inline-container">
+            <h2>My Todos</h2>
+            <button className="addButton" onClick={() => setIsModalOpen(true)}>
               Add Todo
-            </button></span>
+            </button>
+          </span>
         </div>
-      
-      <ol>
-        {todos.map(todo => (
-          <li key={todo._id}>
-            {todo.title} - {todo.description} - <input
+
+        <ol>
+          {todos.map((todo) => (
+            <li key={todo._id}>
+              {todo.title} - {todo.description} -{" "}
+              <input
                 type="checkbox"
                 checked={todo.done} // Set checkbox state based on the todo's done property
                 // onChange={() => handleCheckboxChange(todo._id, !todo.done)}
               />
-          </li>
-        ))}
-      </ol>
+              <button>
+                <MdDelete
+                  entryId={todo._id}
+                  onClick={() => handleDeleteClick(todo._id)}
+                />
+              </button>
+            </li>
+          ))}
+        </ol>
       </div>
       <Modal
         isOpen={isModalOpen}
         onClose={() => setIsModalOpen(false)}
-       
-        
         setTodos={setTodos}
-       
-      >
-       
-      </Modal>
+      ></Modal>
     </div>
   );
 }
